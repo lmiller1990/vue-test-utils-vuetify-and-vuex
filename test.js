747b10a6ca467b29411d8149ce9f0f66a8954c7f
Lachlan <lachlan.miller.1990@outlook.com>
2018-07-18 17:01:20 +0900

    Use plugin to repro bug

--- test.js
+++ test.js
@@ -3,37 +3,35 @@
 import { createLocalVue,shallowMount } from '@vue/test-utils'
 import SimpleComponent from './SimpleComponent'
 import Vuetify from 'vuetify'
-import Vuex from 'vuex'
+
+const myPlugin = {
+  install: function(Vue, opts) {
+    Vue.mixin({
+      // this causes the problem
+    })
+  }
+}
 
 describe('SimpleComponent', () => {
-    it('example test with vuetify first', () => {
-        let localVue = createLocalVue()
-        localVue.use(Vuetify)
-        localVue.use(Vuex)
+  it('example test with vuetify first', () => {
+    let localVue = createLocalVue()
+    localVue.use(Vuetify)
+    localVue.use(myPlugin)
 
-        const wrapper = shallowMount(SimpleComponent, {localVue})
+    const wrapper = shallowMount(SimpleComponent, {localVue})
 
-        console.log("-- Vuetify first --")
-        console.log(wrapper.html())
-    })
+    console.log("-- Vuetify first --")
+    console.log(wrapper.html())
+  })
 
-    it('example test with vuetify second', () => {
-        let localVue = createLocalVue()
-        localVue.use(Vuex)
-        localVue.use(Vuetify)
+  it('example test with vuetify second', () => {
+    let localVue = createLocalVue()
+    localVue.use(myPlugin)
+    localVue.use(Vuetify)
 
-        const wrapper = shallowMount(SimpleComponent, {localVue})
+    const wrapper = shallowMount(SimpleComponent, {localVue})
 
-        console.log("-- Vuetify second --")
-        console.log(wrapper.html())
-    })
-
-    it('example test without vuetify', () => {
-        let localVue = createLocalVue()
-
-        const wrapper = shallowMount(SimpleComponent, {localVue})
-
-        console.log("-- Without Vuetify --")
-        console.log(wrapper.html())
-    })
+    console.log("-- Vuetify second --")
+    console.log(wrapper.html())
+  })
 })
